c2b11d2902c0e087e5dd5c0bcfbd53cdb1c1211a
Giovani Aguirre <aguirregio19@gmail.com>
2017-08-28 21:18:49 -0700

    Adding api to manipulate and display

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,7 +1,34 @@
 import React from "react";
+import axios from "axios";
 
 
 export default class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      type: "all-meat",
+      paras: 4,
+      html: 'text',
+      text: ''
+    }
+  }
+
+  componentWillMount(){
+    this.getSampleText();
+  }
+
+  getSampleText(){
+    axios.get("https://baconipsum.com/api/?type=" + this.state.type  + "&paras=" + this.state.paras + "&format=" + this.state.html)
+      .then((response) => {
+        this.setState({ text: response.data}, () => {
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <div class="home">
